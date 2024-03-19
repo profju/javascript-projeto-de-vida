6a37a47bb4839299c31d7061ade92851497bfc69
profju <124921115+profju@users.noreply.github.com>
2024-03-19 19:23:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 
-//Iterando sobre a lista de botões
-for(let i=0; i<botoes.length; i++){
-    botoes[i].onclick = function(){
-        for (let j = 0; j< botoes.length; j++){
-            botoes[j].classList.remove("ativo");
-              textos[j].classList.remove("ativo");
-        }
-      botoes[i].classList.add("ativo"); 
-        textos[i].classList.add("ativo");
+// Iterando sobre a lista de botões
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
     }
+    botoes[i].classList.add("ativo");
+  };
 }
