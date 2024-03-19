48bd39c294ff5769755b9409b175b5eedc7fd3fe
profju <124921115+profju@users.noreply.github.com>
2024-03-19 19:48:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,4 +15,6 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoAtual = new Date();//retorna a data atual do computador
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
