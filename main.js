f301b30d699fc657bc52a99462c384d7983da040
profju <124921115+profju@users.noreply.github.com>
2024-04-02 19:26:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -23,8 +23,6 @@ let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos / 60);
 let dias = Math.floor(horas / 24);
-console.log(tempoFinal);
-console.log(segundos);
 
-
-
+segundos %=60;
+minutos %=60;
