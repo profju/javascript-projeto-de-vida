0cc75e31d83f1ef6e74575b3c9a0f71a2979ad55
profju <124921115+profju@users.noreply.github.com>
2024-04-02 19:34:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -27,3 +27,5 @@ let dias = Math.floor(horas / 24);
 segundos %=60;
 minutos %=60;
 horas %= 24;
+
+contadores[0].textContent = dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
