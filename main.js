1c20799ca6383b314008fab29f5173fc49b22ddf
profju <124921115+profju@users.noreply.github.com>
2024-04-02 19:17:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ const tempoObjetivo1 = new Date("2024-05-02T00:00:00");
 
 let tempoFinal = tempoObjetivo1 - tempoAtual;
 
-let segundos = Math.floor(tempoFinal / 1000;
-let minutos = Math.floor(segundos / 60;
-let horas = Math.floor(minutos / 60;
-let dias = Math.floor(horas / 24;
+let segundos = Math.floor(tempoFinal / 1000);
+let minutos = Math.floor(segundos / 60);
+let horas = Math.floor(minutos / 60);
+let dias = Math.floor(horas / 24);
 console.log(tempoFinal);
 console.log(segundos);
 
